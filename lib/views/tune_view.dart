21968a2bb02769b98f3d0d1586fbd55d16e0ba6f
Omar <Omarwaleed112005@gmail.com>
2025-06-27 04:22:35 +0300

    change color

--- lib/views/tune_view.dart
+++ lib/views/tune_view.dart
@@ -6,7 +6,7 @@ class TuneView extends StatelessWidget {
   const TuneView({super.key});
 
   final List<TuneModel> tunes = const [
-    TuneModel(color: Colors.black26, sound: 'note1.wav'),
+    TuneModel(color: Colors.white, sound: 'note1.wav'),
     TuneModel(color: Color(0xffF89800), sound: 'note2.wav'),
     TuneModel(color: Color(0xffFEEB3B), sound: 'note3.wav'),
     TuneModel(color: Color(0xff4CAF50), sound: 'note4.wav'),
